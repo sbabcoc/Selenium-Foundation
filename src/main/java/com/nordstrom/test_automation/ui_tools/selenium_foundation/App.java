49745d1e192c4c07e2f0988e1321323c4c74058b
Babcock <BB8D@nordstrom.net>
2017-02-03 15:44:48 -0800

    Initial checkin

---
+++ src/main/java/com/nordstrom/test_automation/ui_tools/selenium_foundation/App.java
@@ -0,0 +1,13 @@
+package com.nordstrom.test_automation.ui_tools.selenium_foundation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
