db224e824990811ed95af06fb2281faad97e4284
BB8D <BB8D@MW00973298.nordstrom.net>
2017-08-04 11:28:39 -0700

    Throw an exception if no driver is found in the current test context

--- src/main/java/com/nordstrom/automation/selenium/exceptions/InitialPageNotSpecifiedException.java
+++ src/main/java/com/nordstrom/automation/selenium/exceptions/InitialPageNotSpecifiedException.java
@@ -4,7 +4,6 @@ public class InitialPageNotSpecifiedException extends RuntimeException {
 
 	private static final long serialVersionUID = -6182879162513331011L;
 
-
 	public InitialPageNotSpecifiedException(String message) {
 		super(message);
 	}
