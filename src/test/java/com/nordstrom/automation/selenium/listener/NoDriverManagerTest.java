676db6f9599e5cfa14f7486e5fce615d8fe94fa0
BB8D <BB8D@MW00973298.nordstrom.net>
2017-08-04 12:00:13 -0700

    More unit test fixes

--- src/test/java/com/nordstrom/automation/selenium/listener/NoDriverManagerTest.java
+++ src/test/java/com/nordstrom/automation/selenium/listener/NoDriverManagerTest.java
@@ -1,34 +1,46 @@
 package com.nordstrom.automation.selenium.listener;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.nordstrom.automation.selenium.annotations.InitialPage;
 import com.nordstrom.automation.selenium.annotations.NoDriver;
+import com.nordstrom.automation.selenium.annotations.PageUrl;
+import com.nordstrom.automation.selenium.exceptions.DriverNotAvailableException;
 import com.nordstrom.automation.selenium.listeners.DriverManager;
 import com.nordstrom.automation.testng.ExecutionFlowController;
 import com.nordstrom.automation.testng.ListenerChain;
 import com.nordstrom.automation.testng.ListenerChainable;
 
 @Listeners({ListenerChain.class})
+@InitialPage(pageUrl=@PageUrl(scheme="file", value="ExamplePage.html"))
 public class NoDriverManagerTest implements ListenerChainable {
 
 	@BeforeMethod(groups = {"NoDriverBefore"})
 	public void beforeMethodNoDriver() {
-		Assert.assertNull(DriverManager.getDriver(), "Driver should not have been created");
+		Assert.assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
 	}
 	
 	@NoDriver
 	@Test(groups = {"NoBeforeNoDriver"})
 	public void testNoBeforeNoDriver() {
-		Assert.assertNull(DriverManager.getDriver(), "Driver should not have been created");
+		Assert.assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
 	}
 	
 	@Test(groups = {"NoDriverBefore"})
 	public void testNoDriverBefore() {
-		Assert.assertNotNull(DriverManager.getDriver(), "Driver should have been created");
+		Assert.assertNotNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should have been created");
 	}
+	
+	@NoDriver
+	@Test(groups = {"NoBeforeNoDriver"}, expectedExceptions = {DriverNotAvailableException.class})
+	public void testNoDriverException() {
+		DriverManager.getDriver();
+	}
+	
 	
 	@Override
 	public void attachListeners(ListenerChain listenerChain) {
