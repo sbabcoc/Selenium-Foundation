4f4d44f5bd919b00ace8008591b749db9ace55ac
bb8d <bb8d@MW00973298.nordstrom.net>
2017-08-23 18:37:40 -0700

    Use static imports for Assert methods

--- src/test/java/com/nordstrom/automation/selenium/listener/NoDriverManagerTest.java
+++ src/test/java/com/nordstrom/automation/selenium/listener/NoDriverManagerTest.java
@@ -1,6 +1,8 @@
 package com.nordstrom.automation.selenium.listener;
 
-import org.testng.Assert;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,18 +18,18 @@ public class NoDriverManagerTest {
 
     @BeforeMethod(groups = {"NoDriverBefore"})
     public void beforeMethodNoDriver() {
-        Assert.assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
+        assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
     }
     
     @NoDriver
     @Test(groups = {"NoBeforeNoDriver"})
     public void testNoBeforeNoDriver() {
-        Assert.assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
+        assertNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should not have been created");
     }
     
     @Test(groups = {"NoDriverBefore"})
     public void testNoDriverBefore() {
-        Assert.assertNotNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should have been created");
+        assertNotNull(DriverManager.getDriver(Reporter.getCurrentTestResult()), "Driver should have been created");
     }
     
     @NoDriver
