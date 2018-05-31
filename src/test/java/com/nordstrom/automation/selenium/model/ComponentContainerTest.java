962f926d75535b719a1efc2ababb21792f248f27
Scott Babcock <scoba@hotmail.com>
2018-05-31 12:49:10 -0700

    Remove unused import

--- src/test/java/com/nordstrom/automation/selenium/model/ComponentContainerTest.java
+++ src/test/java/com/nordstrom/automation/selenium/model/ComponentContainerTest.java
@@ -3,7 +3,6 @@ package com.nordstrom.automation.selenium.model;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.io.File;
