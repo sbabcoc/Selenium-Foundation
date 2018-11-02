77214ef88d4c47a532f7f014c8b5ccf15a38ca08
Scott Babcock <scoba@hotmail.com>
2018-11-02 14:33:55 -0700

    Reformat

--- src/main/java-s2/com/nordstrom/automation/selenium/plugins/HtmlUnitPlugin.java
+++ src/main/java-s2/com/nordstrom/automation/selenium/plugins/HtmlUnitPlugin.java
@@ -184,27 +184,19 @@ public class HtmlUnitPlugin implements DriverPlugin {
     private static final String[] DEPENDENCY_CONTEXTS = {
                     "org.openqa.selenium.htmlunit.HtmlUnitDriver",
                     "com.gargoylesoftware.htmlunit.Version",
-                    "org.apache.commons.collections.ListUtils",
-                    "org.apache.xalan.Version",
-                    "org.apache.xml.serializer.Version",
-                    "org.apache.commons.lang3.CharSet",
-                    "org.apache.http.client.HttpClient",
-                    "org.apache.http.entity.mime.MIME",
+                    "org.apache.commons.collections.ListUtils", "org.apache.xalan.Version",
+                    "org.apache.xml.serializer.Version", "org.apache.commons.lang3.CharSet",
+                    "org.apache.http.client.HttpClient", "org.apache.http.entity.mime.MIME",
                     "org.apache.commons.codec.Encoder",
                     "net.sourceforge.htmlunit.corejs.javascript.Icode",
                     "net.sourceforge.htmlunit.cyberneko.LostText",
-                    "org.apache.xerces.parsers.XMLParser",
-                    "org.apache.xmlcommons.Version",
-                    "com.steadystate.css.parser.Token",
-                    "org.w3c.css.sac.Parser",
-                    "org.apache.commons.io.IOUtils",
-                    "org.apache.commons.logging.Log",
+                    "org.apache.xerces.parsers.XMLParser", "org.apache.xmlcommons.Version",
+                    "com.steadystate.css.parser.Token", "org.w3c.css.sac.Parser",
+                    "org.apache.commons.io.IOUtils", "org.apache.commons.logging.Log",
                     "org.eclipse.jetty.websocket.client.WebSocketClient",
-                    "org.eclipse.jetty.util.IO",
-                    "org.eclipse.jetty.io.EndPoint",
+                    "org.eclipse.jetty.util.IO", "org.eclipse.jetty.io.EndPoint",
                     "org.eclipse.jetty.websocket.common.Parser",
-                    "org.eclipse.jetty.websocket.api.Session"
-                    };
+                    "org.eclipse.jetty.websocket.api.Session"                    };
     
     private static final String BROWSER_NAME = "htmlunit";
 
