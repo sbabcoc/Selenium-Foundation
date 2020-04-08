64914196cd2499c4b516fb7275a325b0839ab2a3
Scott Babcock <scoba@hotmail.com>
2020-04-08 14:38:39 -0700

    Replace tabs with spaces

--- src/main/java/com/nordstrom/automation/selenium/platform/TargetPlatformHandler.java
+++ src/main/java/com/nordstrom/automation/selenium/platform/TargetPlatformHandler.java
@@ -27,22 +27,22 @@ public class TargetPlatformHandler {
         return platform;
     }
     
-	/**
-	 * Determine if the associated method should run on the specified target platform.
-	 * <p>
-	 * <b>NOTE</b>: The method is runnable if the test class implements {@link PlatformTargetable} and the method
-	 * supports the specified target platform. If the method has no {@link TargetPlatform @TargetPlatform} annotation,
-	 * it is assumed to support the implementation-defined 'default' platform. If the test class doesn't implement
-	 * <b>PlatformTargetable</b>, the method is runnable if the CONTEXT_PLATFORM setting matches the specified target
-	 * platform.
-	 * 
-	 * @param contextPlatform active context platform
-	 * @param platformConstant {@link PlatformEnum} constant for the current method
+    /**
+     * Determine if the associated method should run on the specified target platform.
+     * <p>
+     * <b>NOTE</b>: The method is runnable if the test class implements {@link PlatformTargetable} and the method
+     * supports the specified target platform. If the method has no {@link TargetPlatform @TargetPlatform} annotation,
+     * it is assumed to support the implementation-defined 'default' platform. If the test class doesn't implement
+     * <b>PlatformTargetable</b>, the method is runnable if the CONTEXT_PLATFORM setting matches the specified target
+     * platform.
+     * 
+     * @param contextPlatform active context platform
+     * @param platformConstant {@link PlatformEnum} constant for the current method
      * @return 'true' if the associated method should run on the specified target platform; otherwise 'false'
-	 */
+     */
     public static boolean shouldRun(String contextPlatform, PlatformEnum platformConstant) {
         if ((contextPlatform != null) && (platformConstant != null)) {
-    		return platformConstant.matches(contextPlatform);
+            return platformConstant.matches(contextPlatform);
         }
         return true;
     }
