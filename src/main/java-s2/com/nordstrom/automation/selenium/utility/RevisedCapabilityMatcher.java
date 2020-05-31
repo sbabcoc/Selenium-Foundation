d4f839c346b7875c3681a5a99aeacfaf1b3b57fa
Scott Babcock <scoba@hotmail.com>
2020-05-30 22:56:39 -0700

    Remove do-nothing subclass

--- src/main/java-s2/com/nordstrom/automation/selenium/utility/RevisedCapabilityMatcher.java
+++
@@ -1,6 +0,0 @@
-package com.nordstrom.automation.selenium.utility;
-
-import org.openqa.grid.internal.utils.DefaultCapabilityMatcher;
-
-public class RevisedCapabilityMatcher extends DefaultCapabilityMatcher {
-}
