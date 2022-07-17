c3f84029b8f974f3b77d9421abee6f8c49ea1285
Scott Babcock <scoba@hotmail.com>
2022-07-17 09:21:55 -0700

    Add Windows page/test; catch WebDriverException

---
+++ src/test/java/com/nordstrom/automation/selenium/windows/WindowsTest.java
@@ -0,0 +1,26 @@
+package com.nordstrom.automation.selenium.windows;
+
+import static com.nordstrom.automation.selenium.platform.TargetType.WINDOWS_NAME;
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import com.nordstrom.automation.selenium.examples.WindowsPage;
+import com.nordstrom.automation.selenium.annotations.InitialPage;
+import com.nordstrom.automation.selenium.examples.TestNgTargetRoot;
+import com.nordstrom.automation.selenium.platform.TargetPlatform;
+
+@InitialPage(WindowsPage.class)
+public class WindowsTest extends TestNgTargetRoot {
+    
+    @Test
+    @TargetPlatform(WINDOWS_NAME)
+    public void testEditing() {
+        WindowsPage page = getInitialPage();
+        page.modifyDocument("Hello world!");
+        assertEquals(page.getDocument(), "Hello world!");
+        page.modifyDocument(Keys.CONTROL + "z");
+    }
+    
+}
