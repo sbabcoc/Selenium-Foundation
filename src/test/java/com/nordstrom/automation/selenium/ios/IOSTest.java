4bba2716672708a7a1605ef06d20fab10b6782d1
Scott Babcock <scoba@hotmail.com>
2022-08-09 23:00:05 -0700

    Add XCUITest node plugin

---
+++ src/test/java/com/nordstrom/automation/selenium/ios/IOSTest.java
@@ -0,0 +1,24 @@
+package com.nordstrom.automation.selenium.ios;
+
+import static com.nordstrom.automation.selenium.platform.TargetType.IOS_APP_NAME;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.nordstrom.automation.selenium.annotations.InitialPage;
+import com.nordstrom.automation.selenium.examples.TestNgTargetRoot;
+import com.nordstrom.automation.selenium.examples.IOSPage;
+import com.nordstrom.automation.selenium.platform.TargetPlatform;
+
+@InitialPage(IOSPage.class)
+public class IOSTest extends TestNgTargetRoot {
+    
+    @Test
+    @TargetPlatform(IOS_APP_NAME)
+    public void testEditing() {
+        IOSPage page = getInitialPage();
+        page.modifyField("Hello world!");
+        assertEquals(page.accessField(), "Hello world!");
+    }
+    
+}
