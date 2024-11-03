59490aaf8354a535b34303cd728412f6d80dda15
Scott Babcock <scoba@hotmail.com>
2024-11-03 11:25:45 -0800

    S3: servlets only for hub; S4: add servlet support

--- src/selenium4/java/com/nordstrom/automation/selenium/examples/ServletContainer.java
+++ src/selenium4/java/com/nordstrom/automation/selenium/examples/ServletContainer.java
@@ -1,10 +1,15 @@
 package com.nordstrom.automation.selenium.examples;
 
+import com.beust.jcommander.JCommander;
+import com.nordstrom.automation.selenium.SeleniumConfig;
 import com.nordstrom.automation.selenium.servlet.ExamplePageServlet;
 import com.nordstrom.automation.selenium.servlet.ExamplePageServlet.FrameA_Servlet;
 import com.nordstrom.automation.selenium.servlet.ExamplePageServlet.FrameB_Servlet;
 import com.nordstrom.automation.selenium.servlet.ExamplePageServlet.FrameC_Servlet;
 import com.nordstrom.automation.selenium.servlet.ExamplePageServlet.FrameD_Servlet;
+
+import java.util.Collections;
+import java.util.Set;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -20,20 +25,29 @@ public class ServletContainer {
         "org.eclipse.jetty.security.SecurityHandler",
         "org.eclipse.jetty.server.Server",
         "org.eclipse.jetty.servlet.ServletHolder",
-        "org.eclipse.jetty.util.component.LifeCycle"
+        "org.eclipse.jetty.util.component.LifeCycle",
+        "com.beust.jcommander.JCommander"
     };
     
     public static String[] getDependencyContexts() {
-        return DEPENDENCY_CONTEXTS;
+        SeleniumConfig config = SeleniumConfig.getConfig();
+        Set<String> servlets = config.getGridServlets();
+        Collections.addAll(servlets, DEPENDENCY_CONTEXTS);
+        return servlets.toArray(new String[0]);
     }
     
     public static void main(String[] args) throws Exception {
+        ServletFlags flags = new ServletFlags();
+        JCommander comamnder = JCommander.newBuilder().addObject(flags).build();
+        
         int port = 8080;
         
         Server server = new Server(port);
 
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
+        
+        
         
         context.addServlet(new ServletHolder(new ExamplePageServlet()), "/grid/admin/ExamplePageServlet");
         context.addServlet(new ServletHolder(new FrameA_Servlet()), "/grid/admin/FrameA_Servlet");
